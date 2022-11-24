083eb222059d0900f68088dd5fdf9773746db3eb
Paulus Potter <paulus@tecnion.nl>
2022-11-24 15:01:37 -0500

    Moved console functions into file

--- main/main.c
+++ main/main.c
@@ -47,6 +47,7 @@ esp_err_t start_rest_server(const char *base_path);
 
 void task_hmi(void* ignore);
 void task_logging(void * pvParameters);
+void init_console();
 
 /*
 This ISR is called when the handshake line goes high.
@@ -218,134 +219,14 @@ static void IRAM_ATTR gpio_handshake_isr_handler(void* arg)
 
 
 
-static struct {
-    struct arg_int *adc_en_level;
-    struct arg_end *end;
-} adc_en_args;
-
-static int adc_enable_pin(int argc, char **argv)
-{
-    int nerrors = arg_parse(argc, argv, (void **) &adc_en_args);
-    if (nerrors != 0) {
-        arg_print_errors(stderr, adc_en_args.end, argv[0]);
-        return 1;
-    }
-    if (adc_en_args.adc_en_level) {
-        
-        ESP_LOGI(TAG, "ADC OUTPUT %d", adc_en_args.adc_en_level->ival[0]);
-        gpio_set_level(GPIO_ADC_EN, adc_en_args.adc_en_level->ival[0]);
-        // ESP_ERROR_CHECK( esp_sleep_enable_timer_wakeup(timeout) );
-    }
-
-    return 0;
-}
-
-static void register_restart(void)
-{
- int num_args = 0;
-    
-    adc_en_args.end = arg_end(num_args);
-
-    const esp_console_cmd_t cmd = {
-        .command = "restart",
-        .help = "Restart ESP32",
-        .hint = NULL,
-        .func = &esp_restart
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-static void register_adc_en_pin(void)
-{
- int num_args = 1;
-    adc_en_args.adc_en_level =
-        
-    arg_int0(NULL, NULL, "<0|1>", "GPIO level to trigger wakeup");
-    adc_en_args.end = arg_end(num_args);
-
-    const esp_console_cmd_t cmd = {
-        .command = "adc_en",
-        .help = "Control adc enable pin",
-        .hint = NULL,
-        .func = &adc_enable_pin,
-        .argtable = &adc_en_args
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-// static int sdcard_testwrite(int argc, char **argv)
-// {
-//     if(esp_sd_card_write(tbuffer, 4*1024))
-//     {
-//         ESP_LOGI(TAG, "Succesfull write");
-//         return 0;
-//     } else {
-//         ESP_LOGE(TAG, "failed to write to file!");
-//     }
-//     return 1;
-// }
-
-static void register_sd_card_init(void)
-{
-     const esp_console_cmd_t cmd = {
-        .command = "sd_init",
-        .help = "Initialize SD card and file",
-        .hint = NULL,
-        .func = &esp_sd_card_init,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-static void register_sd_card_close(void)
-{
-    int num_args = 0;    
-    adc_en_args.end = arg_end(num_args);
-
-    const esp_console_cmd_t cmd = {
-        .command = "sd_close",
-        .help = "Close file and unmount sd card",
-        .hint = NULL,
-        .func = &esp_sd_card_close_unmount,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-
 
 //Main application
 void app_main(void)
 {
-    // static uint8_t ucParameterToPass;
-
-    esp_console_repl_t *repl = NULL;
-    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
-     */
-    repl_config.prompt = PROMPT_STR ">";
-    // repl_config.max_cmdline_length = CONFIG_CONSOLE_MAX_COMMAND_LINE_LENGTH;
-    #if CONFIG_CONSOLE_STORE_HISTORY
-    initialize_filesystem();
-    repl_config.history_save_path = HISTORY_PATH;
-    ESP_LOGI(TAG, "Command history enabled");
-    #else
-    ESP_LOGI(TAG, "Command history disabled");
-    #endif
 
     // Register console commands
-    register_adc_en_pin();
-    register_sd_card_close();
-    register_restart();
-    register_sd_card_init();
-    esp_console_register_help_command();
-
-    // esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-    esp_console_dev_uart_config_t hw_config =    ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
-
-    // Enable console
-    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));
-    ESP_ERROR_CHECK(esp_console_start_repl(repl));
-
+    init_console();
+    
     //GPIO config for the handshake line.
     gpio_config_t io_conf={
         .intr_type=GPIO_INTR_POSEDGE,
