17812bb455bae5c91c431e7f0d4d133a32d247a9
Paulus Potter <ppotter10@gmail.com>
2022-12-06 17:08:31 +0100

    Added samplerates to settings

--- main/settings.c
+++ main/settings.c
@@ -5,7 +5,7 @@ Settings_t _settings;
 void settings_init()
 {
     _settings.adc_resolution = ADC_12_BITS;
-    _settings.log_sample_rate = 1; // 1 Hz 
+    _settings.log_sample_rate = ADC_SAMPLE_RATE_10Hz; // 10Hz 
     _settings.adc_channel_type = 0x00; // all channels normal ADC by default
     _settings.logMode = LOGMODE_CSV;
 }
@@ -22,5 +22,23 @@ log_mode_t settings_get_logmode()
 uint8_t settings_set_logmode(log_mode_t mode)
 {
     _settings.logMode = mode;
+
     return RET_OK;
+}
+
+uint8_t settings_set_samplerate(adc_sample_rate_t rate)
+{
+    if (rate > 0 && rate < ADC_SAMPLE_RATE_NUM_ITEMS)
+    {
+        _settings.log_sample_rate = rate;
+        return RET_OK;
+    } else {
+        return RET_NOK;
+    }
+    
+}
+
+adc_sample_rate_t settings_get_samplerate()
+{
+    return _settings.log_sample_rate;
 }
--- main/settings.h
+++ main/settings.h
@@ -9,9 +9,35 @@
 #include "common.h"
 
 enum adc_resolution_e {
-    ADC_12_BITS = 0,
-    ADC_16_BITS = 1
+    ADC_12_BITS = 1,
+    ADC_16_BITS = 2
 };
+
+typedef enum adc_sample_rate_e {
+    ADC_SAMPLE_RATE_1Hz = 1,
+	ADC_SAMPLE_RATE_10Hz,
+	ADC_SAMPLE_RATE_25Hz,
+	ADC_SAMPLE_RATE_50Hz,
+	ADC_SAMPLE_RATE_100Hz,
+	ADC_SAMPLE_RATE_200Hz,
+	ADC_SAMPLE_RATE_400Hz,
+	ADC_SAMPLE_RATE_500Hz,
+	ADC_SAMPLE_RATE_1000Hz,
+	ADC_SAMPLE_RATE_2000Hz,
+	ADC_SAMPLE_RATE_4000Hz,
+	ADC_SAMPLE_RATE_5000Hz,
+	ADC_SAMPLE_RATE_8000Hz,
+	ADC_SAMPLE_RATE_10000Hz,
+	ADC_SAMPLE_RATE_20000Hz,
+	ADC_SAMPLE_RATE_40000Hz,
+	ADC_SAMPLE_RATE_50000Hz,
+	ADC_SAMPLE_RATE_100000Hz,
+	ADC_SAMPLE_RATE_250000Hz,
+	ADC_SAMPLE_RATE_500000Hz,
+	ADC_SAMPLE_RATE_1000000Hz,
+    ADC_SAMPLE_RATE_NUM_ITEMS
+} adc_sample_rate_t;
+
 
 typedef enum log_mode_e {
     LOGMODE_RAW = 0,
@@ -21,7 +47,7 @@ typedef enum log_mode_e {
 
 struct Settings_t {
     uint8_t adc_resolution;
-    uint32_t log_sample_rate; // can make this one out of fixed options
+    adc_sample_rate_t log_sample_rate; // can make this one out of fixed options
     uint8_t adc_channel_type; // indicate whether channel 0..7 are normal ADC (bit = 0) or NTC (bit = 1). LSB = channel 0, MSB = channel 7
     uint8_t logMode;
 };
@@ -34,5 +60,8 @@ void settings_init();
 Settings_t * settings_get();
 log_mode_t settings_get_logmode();
 uint8_t settings_set_logmode(log_mode_t mode);
+void settings_set_samplerate(adc_sample_rate_t rate);
+adc_sample_rate_t settings_get_samplerate(void);
+
 
 #endif
