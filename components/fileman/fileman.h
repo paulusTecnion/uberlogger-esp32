f5a507f8ffc42676dfec9083fa0ac69cf91a25ab
Paulus Potter <ppotter10@gmail.com>
2023-01-20 15:00:36 +0100

    Writing header to csv 2, missing header

--- components/fileman/fileman.h
+++ components/fileman/fileman.h
@@ -12,4 +12,4 @@ esp_err_t fileman_open_file(void);
 esp_err_t fileman_close_file(void);
 esp_err_t fileman_search_last_sequence_file(void);
 int fileman_write(const void * data, size_t len);
-int fileman_csv_write(const void * data, size_t len);
+int fileman_csv_write_header(void);
