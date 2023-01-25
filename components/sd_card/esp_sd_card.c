f20cfe77f1cfc3523b50957aec54c77a71a22fb4
Paulus Potter <ppotter10@gmail.com>
2023-01-25 12:04:25 +0100

    sdcard cleanup

--- components/sd_card/esp_sd_card.c
+++ components/sd_card/esp_sd_card.c
@@ -188,65 +188,10 @@ esp_err_t esp_sd_card_init(void)
     } else {
         esp_sd_spi_is_initialized = true;
     }
-
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
-    // sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
-    // slot_config.gpio_cs = PIN_NUM_CS;
-    // slot_config.host_id = host.slot;
-
-  
     
 #endif //USE_SPI_MODE
 
     
-
-    // Card has been initialized, print its properties
-    // sdmmc_card_print_info(stdout, card);
-
-   
-//     fprintf(f, "Hello %s!\n", card->cid.name);
-//     fclose(f);
-//     ESP_LOGI(TAG, "File written");
-
-//     // Check if destination file exists before renaming
-//     struct stat st;
-//     if (stat(MOUNT_POINT"/foo.txt", &st) == 0) {
-//         // Delete it if it exists
-//         unlink(MOUNT_POINT"/foo.txt");
-//     }
-
-//     // Rename original file
-//     ESP_LOGI(TAG, "Renaming file");
-//     if (rename(MOUNT_POINT"/hello.txt", MOUNT_POINT"/foo.txt") != 0) {
-//         ESP_LOGE(TAG, "Rename failed");
-//         return;
-//     }
-
-//     // Open renamed file for reading
-//     ESP_LOGI(TAG, "Reading file");
-//     f = fopen(MOUNT_POINT"/foo.txt", "r");
-//     if (f == NULL) {
-//         ESP_LOGE(TAG, "Failed to open file for reading");
-//         return;
-//     }
-//     char line[64];
-//     fgets(line, sizeof(line), f);
-//     fclose(f);
-//     // strip newline
-//     char* pos = strchr(line, '\n');
-//     if (pos) {
-//         *pos = '\0';
-//     }
-//     ESP_LOGI(TAG, "Read from file: '%s'", line);
-
-//     // All done, unmount partition and disable SDMMC or SPI peripheral
-//     esp_vfs_fat_sdcard_unmount(mount_point, card);
-//     ESP_LOGI(TAG, "Card unmounted");
-// #ifdef USE_SPI_MODE
-//     //deinitialize the bus after all devices are removed
-//     spi_bus_free(host.slot);
-// #endif
     return ESP_OK;
     
 }
