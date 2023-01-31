7bf84e7efb9ba7504737840382ccc4b4f3a01f80
Paulus Potter <ppotter10@gmail.com>
2023-01-31 14:24:10 +0100

    Mode button works as on/off button

--- main/logger.h
+++ main/logger.h
@@ -121,13 +121,17 @@ esp_err_t Logger_log();
 esp_err_t Logger_start();
 esp_err_t Logger_stop();
 
+
 // uint8_t Logger_flush_buffer_to_sd_card();
 uint8_t Logger_flush_buffer_to_sd_card_uint8(uint8_t * buffer, size_t size);
 uint8_t Logger_flush_buffer_to_sd_card_csv(int32_t * adcData, size_t lenAdcBytes, uint8_t * gpioData, size_t lenGpio, uint8_t * timeData, size_t lenTime);
 
 uint8_t Logger_isLogging(void);
+
 uint8_t Logger_setCsvLog(log_mode_t value);
 uint8_t Logger_getCsvLog();
+
+uint8_t Logger_mode_button();
 uint8_t Logger_syncSettings();
 uint8_t Logger_sendSTM32cmd(stm32cmd_t cmd);
 
