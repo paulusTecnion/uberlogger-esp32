5d9d63e00079375c7a31dd8bf05bb82214080da8
Paulus Potter <paulus@tecnion.nl>
2023-04-21 21:41:59 +0200

    Auth failed / print ip / get rssi functions

--- main/rest_server.c
+++ main/rest_server.c
@@ -567,6 +567,22 @@ static esp_err_t Logger_stop_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+static esp_err_t logger_wifi_status_handler(httpd_req_t *req)
+{
+    httpd_resp_set_type(req, "application/json");
+    cJSON *root = cJSON_CreateObject();
+
+    cJSON_AddBoolToObject(root, (const char*)"connected", wifi_is_connected());
+    cJSON_AddStringToObject(root, (const char*)"ssid", wifi_get_ssid());
+    cJSON_AddStringToObject(root, (const char*)"ip", wifi_get_ip());
+
+    const char *sys_info = cJSON_Print(root);
+    httpd_resp_sendstr(req, sys_info);
+    free((void*)sys_info);
+    cJSON_Delete(root);
+    return ESP_OK;
+}
+
 esp_err_t json_send_resp(httpd_req_t *req, endpoint_response_t type, char * reason)
 {
 
@@ -624,8 +640,13 @@ esp_err_t start_rest_server(const char *base_path)
     ESP_LOGI(REST_TAG, "Starting HTTP Server");
     REST_CHECK(httpd_start(&server, &config) == ESP_OK, "Start server failed", err_start);
 
-
-
+    httpd_uri_t logger_wifi_status_uri = {
+        .uri = "/ajax/wifiStatus",
+        .method = HTTP_GET,
+        .handler = logger_wifi_status_handler,
+        .user_ctx = rest_context
+    };
+    }
 
     httpd_uri_t logger_getConfig_uri = {
         .uri = "/ajax/getConfig",
