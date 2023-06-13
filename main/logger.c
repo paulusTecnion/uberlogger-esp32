51ad4811449f1e66f1fbae3c1af9f23ee66760c9
Paulus Potter <paulus@tecnion.nl>
2023-06-13 15:50:35 +0200

    Longer sync time time

--- main/logger.c
+++ main/logger.c
@@ -366,7 +366,7 @@ esp_err_t Logger_syncSettings(uint8_t syncTime)
 
     Logger_resetSTM32();
 
-
+    vTaskDelay(300 / portTICK_PERIOD_MS);
     // Send command to STM32 to go into settings mode
     #ifdef DEBUG_LOGGING
     ESP_LOGI(TAG_LOG, "Setting SETTINGS mode");
