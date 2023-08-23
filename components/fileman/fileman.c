82d2a6727c8b70369963a857b16603aa36c54134
Paulus Potter <paulus@tecnion.nl>
2023-08-23 20:21:08 +0200

    Fix: temp written wrongly to csv

--- components/fileman/fileman.c
+++ components/fileman/fileman.c
@@ -206,10 +206,8 @@ int fileman_csv_write(const int32_t * dataAdc,  size_t lenAdc, const uint8_t* da
             // If temperature sensor and 16 bits we need to multiply input with 100 (or divide with factor 100 less which is ADC_MULT_FACTOR_16B_TEMP)
             if ((settings_get()->adc_channel_type & (1 << x)))
             {
-                // if ((settings_get()->adc_resolution == ADC_16_BITS))
-                // {
                     // Only 2 digits after decimal point for temperature
-                    writeptr = writeptr + snprintf(filestrbuffer+writeptr, 14, "%s%d.%07d,",
+                    writeptr = writeptr + snprintf(filestrbuffer+writeptr, 14, "%s%d.%06d,",
                         (dataAdc[i*NUM_ADC_CHANNELS+x] < 0) ? "-" : "",
                         abs(dataAdc[i*NUM_ADC_CHANNELS+x] / (ADC_MULT_FACTOR_16B_TEMP)), 
                         abs(dataAdc[i*NUM_ADC_CHANNELS+x] % ADC_MULT_FACTOR_16B_TEMP));
