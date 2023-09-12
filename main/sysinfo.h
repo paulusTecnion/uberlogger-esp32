a48d9bd0a59249f69376a5dc1856ab416da79966
Paulus Potter <paulus@tecnion.nl>
2023-09-12 14:46:44 +0200

    Changed update_version to V0.1.0

--- main/sysinfo.h
+++ main/sysinfo.h
@@ -2,7 +2,7 @@
 
 #include "esp_system.h"
 
-static const char SW_VERSION[] =  "0.2.0_2023.09.06.16.37";
+static const char SW_VERSION[] =  "0.1.0_2023.09.12.14.46";
 
 float sysinfo_get_core_temperature();
 const char * sysinfo_get_fw_version();
