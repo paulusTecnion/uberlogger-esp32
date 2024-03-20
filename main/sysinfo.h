04715fabedab7718193d33da58d7e71f86fcd2f5
Paulus Potter <info@tecnion.nl>
2024-03-20 12:42:13 +0100

    Updated version time

--- main/sysinfo.h
+++ main/sysinfo.h
@@ -2,7 +2,7 @@
 
 #include "esp_system.h"
 
-static const char SW_VERSION[] =  "1.0.3_2024.03.20.09.31";
+static const char SW_VERSION[] =  "1.0.3_2024.03.20.12.35";
 
 // float sysinfo_get_core_temperature();
 const char * sysinfo_get_fw_version();
