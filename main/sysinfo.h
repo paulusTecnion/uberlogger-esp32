90b21f51076169d696a744c7a28e4a436b965c85
Paulus Potter <paulus@tecnion.nl>
2024-03-28 17:08:11 +0000

    Develop

--- main/sysinfo.h
+++ main/sysinfo.h
@@ -2,7 +2,7 @@
 
 #include "esp_system.h"
 
-static const char SW_VERSION[] =  "1.1.1_2024.03.27.21.24";
+static const char SW_VERSION[] =  "1.1.2_2024.03.28.17.53";
 
 // float sysinfo_get_core_temperature();
 const char * sysinfo_get_fw_version();
